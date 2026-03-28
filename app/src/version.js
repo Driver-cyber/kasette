8ebd95324f87048d53f54ac6ba17e594a6951d3b
Driver-cyber <cstewch@gmail.com>
2026-03-27 20:30:14 -0700

    Update app version to 6.5.4

--- app/src/version.js
+++ app/src/version.js
@@ -3,7 +3,7 @@
 
 export const APP_VERSION = {
   // Version number (e.g., "0.4.0", "1.0.0", "2.1.3")
-  number: "0.5.0",
+  number: "6.5.4",
   
   // Build date/info (e.g., "March 2026", "Spring 2026", "2026-03-05")
   build: "March 2026",
